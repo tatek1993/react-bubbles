95b86a6ea083e5d712b654062fa00bb6a43bd1e9
Tate Keller <tatek1993@gmail.com>
2020-02-28 11:47:08 -0600

    created Utils folder and inside of it created axiosWithAuth.js and PrivateRoute.js. Set up useEffect in BubblePage.js and set up saveUpdate and saveEdit inside of ColorList.js
      *put PrivateRoute in App.js, passed BubblePage as a component in PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
 
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
@@ -9,13 +11,23 @@ const Login = () => {
     password: ''
   });
 
+  const history = useHistory();
+
+  const handleChange = e => {
+    e.preventDefault();
+    setLogin({...login, [e.target.name]: e.target.value})
+  }
+
   const handleSubmit = e => {
     e.preventDefault();
-    axios
+    axiosWithAuth()
       .post('/login', login)
       .then(res => {
         console.log('res', res)
+        window.localStorage.setItem('token', res.data.payload)
+        history.push('/bubbles');
       })
+      .catch(err => console.log('there was an error', err.response))
   }
 
   return (
@@ -43,7 +55,7 @@ const Login = () => {
             className="input"
           />     
         </label>
-        <button className="start">Start</button>
+        <button className="start">Login</button>
       </form>
     </div>
   );
